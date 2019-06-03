e276c474bf9bf9d9ceccba472b48a917172862d8
t5krishn <tk95@live.ca>
2019-06-03 18:12:28 -0400

    Updated comments on index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 /*jshint esversion: 8 */
 
+// MAIN FILE OF TINYURL APP
+// REQUIREMENTS: Make an app that takes in a url and an alias(optional)
+//    - and return a link for it that is "shorter" incorporating the 
+//    - alias if there is one given, or generate a unique url
+// STORAGE/HOSTING:  Hosting on heroku, db used is Postgresql on heroku
+// URL: https://t-tinyurl.herokuapp.com
+
 
 
 
@@ -39,8 +46,8 @@ app.get('/', function (req, res) {
 });
 
 
-// Used one time to initalize the tinyurl db table, not needed anymore---------
-// Can be used to initialize new tables if needed------------
+// Used one time to initalize the tinyurl db table, not needed anymore
+// Can be used to initialize new tables if needed
 // app.get('/init', function (req, res) {
 //   const client = new Client({
 //     connectionString: process.env.DATABASE_URL,
@@ -57,7 +64,10 @@ app.get('/', function (req, res) {
 // });
 
 
-// APP GET, 
+// APP GET --> Checks if alias exists in db
+//  - if it does, redirect to link stored in db corresponding to alias
+//  - if not, show an error message and a link to redirect to main page to 
+//  - make a new tinyurl
 app.get('/r/:alias', function (req, response) {
   const client = new Client({
       connectionString: process.env.DATABASE_URL,
@@ -66,10 +76,11 @@ app.get('/r/:alias', function (req, response) {
   client.connect();
 
   client.query('SELECT * FROM tinyurltable WHERE alias=$1', [req.params.alias], (err, res) => {
-    if(res.rowCount == 0){
+    if(res.rowCount == 0){  /* This means that the alias does not exist in db*/
       console.log(err,res);
-      // response.sendFile(__dirname + '/front/index.html');
+
       // Run alert/update page saying that alias is not registered
+      // FUTURE UPDATE: CHANGE TO A HTML PAGE UPDATE/REACT COMPONENT UPDATE ---------------
       response.set('Content-Type', 'text/html');
       response.send(new Buffer(''+
       '<h2> Alias not found</h2>'+
@@ -77,6 +88,8 @@ app.get('/r/:alias', function (req, response) {
       '<p>Return to main page to create a tinyurl</p>'+
       '<br><button onclick="location.href = \'https://t-tinyurl.herokuapp.com\';">tinyurl</button>'));
     }else{
+      // If res.rowCount is not zero, that means there is an alias registered
+      //  - so redirect to the link stored in the db
       console.log(err, res);
       response.redirect(res.rows[0].longurl);
     }
@@ -88,18 +101,20 @@ app.get('/r/:alias', function (req, response) {
 
 
 
-// APP POST GETS DATA FROM FORM IN FRONT/INDEX.HTML
+// APP POST --> GETS DATA FROM FORM IN FRONT/INDEX.HTML
 // DATA IS PARSED WITH BODY-PARSER AS URLENCODED STRING
 // req.body has obj of form {alias: "", url:""}
 // can access it for eg:  req.body.alias 
 app.post('/url',urlencodedParser,function(req,response){
-  console.log(req.body.alias);
+  // console.log(req.body.alias);
 
   if(req.body.alias != ''){/* IF ALIAS IS NOT EMPTY, IF IT'S EMPTY GENERATE A UNIQUE STRING AS ALIAS */
 
+    // Calls inTable fn, that returns a bool, TRUE IF alias is in table, FALSE IF alias is not in table
     inTable(req.body.alias).then(function(inTableResult){
       
       // IF ALIAS SENT IN IS ALREADY IN THE TABLE, THEN SAY IT'S REGISTERED AND SHOW OPTIONS
+      // FUTURE UPDATE: CHANGE TO A HTML PAGE UPDATE/REACT COMPONENT UPDATE ---------------
       if(inTableResult){ 
         response.set('Content-Type', 'text/html');
         response.send(new Buffer(''+
@@ -115,6 +130,7 @@ app.post('/url',urlencodedParser,function(req,response){
         insert(req.body.alias, req.body.url).then(function(insertResult){
           // IF INSERT WORKS, SHOULD RETURN TRUE SO SHOW SUCCESS AND TINYURL LINK
           if(insertResult){
+            // FUTURE UPDATE: CHANGE TO A HTML PAGE UPDATE/REACT COMPONENT UPDATE ---------------
             response.set('Content-Type', 'text/html');
             response.send(new Buffer(''+
             '<h2> Alias registered!</h2>'+
@@ -141,10 +157,12 @@ app.post('/url',urlencodedParser,function(req,response){
   // IF ALIAS IS EMPTY, ELSE START
   else{
     // IF ALIAS IS EMPTY THEN GENERATE AN ALIAS THAT'S UNIQUE(TRIES TO BE)
-    // generate unique id as alias
+    // generate unique string as alias
     var alias = Math.random().toString(36).substr(2, 9);
 
+    // INSERT fn attempts to insert new alias and url into db
     insert(alias, req.body.url).then(function(insertResult){
+  
         if(insertResult){
           response.set('Content-Type', 'text/html');
           response.send(new Buffer(''+
@@ -171,7 +189,7 @@ app.post('/url',urlencodedParser,function(req,response){
 // END OF APP.POST FUNCTION
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Tiny url app listening on port ${port}!`));
 
 
 // async function ID() {
@@ -194,6 +212,8 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 // }
 
 
+// INTABLE function checks to see if the text sent in exists as an
+//   - alias and returns a boolean
 async function inTable(alias) {
   try {
     const client = new Client({
@@ -216,6 +236,8 @@ async function inTable(alias) {
   }
 }
 
+// INSERT function attempts to insert new alias and url into db,
+//  - returns true if successful
 async function insert(alias,url){
   try {
     const client = new Client({
